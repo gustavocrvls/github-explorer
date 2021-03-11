765b1996ddcf452c4c0c382c66764090ef00cbf4
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-03-10 21:31:16 -0300

    Creation of Repository Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import { RespositoryList } from './components/RepositoryList';
+
 import './styles/global.scss';
 
 export function App() {
-    return <h1>Hello World!</h1>
+    return <RespositoryList />
 }
