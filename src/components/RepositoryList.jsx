628922d84d5fac5e73c9bce604fb05635b129737
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-03-11 19:58:10 -0300

    list repositories

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -3,12 +3,6 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
 
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/unform/unform'
-}
-
 export function RespositoryList() {
   const [repositories, setRepositories] = useState([]);
 
@@ -23,10 +17,9 @@ export function RespositoryList() {
       <h1>Lista de Respositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
+        {
+          repositories.map(repository => <RepositoryItem key={repository.name} repository={repository} />)
+        }
       </ul>
     </section>
   )
